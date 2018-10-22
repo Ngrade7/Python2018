b18c2f8254f0e738f7e733d34dae1a0f4e608195
Ngrade7 <42973142+Ngrade7@users.noreply.github.com>
2018-10-22 09:47:52 -0700

    Add files via upload

--- turtle_project.py
+++ turtle_project.py
@@ -5,22 +5,31 @@ import turtle
 
 def star(t,x,y):
     t.goto(x,y)
-    for x in range (0,1440,15):
+    for x in range (0,540,5):
         t.color('#ff0000')
-        t.width(1)
+        t.width(1.5)
         t.forward(x + 0.5)
         t.left(151)
+        t.speed(0)
 
-
+def stem(t,x,y):
+    t.goto(x,y)
+    for x in range (0,360,-100):
+        t.color('#00ff00')
+        t.width(5)
+        t.forward(0)
+        t.down(400)
 
 
 def work():
     tw = turtle.Screen()
     tw.clear()
     t = turtle.Turtle()
-    t.width(3)
+    t.width(5)
     # *******************
-    star(t,10,10)
+    star(t,0,0)
+    stem(t,0,-100)
+
 
     tw.exitonclick()
 
