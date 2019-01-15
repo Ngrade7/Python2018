4bc6b1f6f835497bab06c57cafa6956bf8e720e4
Ngrade7 <42973142+Ngrade7@users.noreply.github.com>
2019-01-15 10:03:14 -0800

    Add files via upload

---
+++ lambda1.py
@@ -0,0 +1,8 @@
+# create three different files using the lambda quadratic_function
+# * * * * * * * * * * * * * *
+
+# file 1 : lambda.py nrg
+x = lambda a, b, c : a + b + c
+fx = x(10,11,12)
+print(fx)
+
---
+++ lambda2.py
@@ -0,0 +1,14 @@
+#file 2: lambda2.py nrg
+
+def myfunc(n):
+	return lambda a : a * n
+	
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+dub = mydoubler(11)
+trip = mytripler(11)
+
+print("dub ",dub)
+print("trip ",trip)
+
+# * * * * * * * * * * * * * *
